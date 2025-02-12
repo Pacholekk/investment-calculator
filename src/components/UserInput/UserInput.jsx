5e31330d801373f44b844484ed13306be50665cf
Pacholekk <patryk.pacholski2001@gmail.com>
2025-02-12 12:08:42 +0100

    Add handlig events in input

--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,26 +1,71 @@
 import "./UserInput.css";
+import { useState } from "react";
 
-export default function UserInput() {
+export default function UserInput({ input }) {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 0,
+    annualInvestment: 0,
+    expecterReturn: 0,
+    duration: 0,
+  });
+  function handleChangeValue(inputIdentifier, value) {
+    setUserInput((prevUserInput) => {
+      //Gets previous state
+
+      return {
+        ...prevUserInput, // Copys all existing values
+        [inputIdentifier]: value, // Updates only the changed field
+      };
+    });
+  }
   return (
     <div id="user-input">
       <div className="input-group">
         <div>
-          <label htmlFor=""> INITIAL INVESTMENT</label>
-          <input type="number" />
+          <label> INITIAL INVESTMENT</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              handleChangeValue("initialInvestment", event.target.value)
+            }
+          />
         </div>
         <div>
-          <label htmlFor=""> ANNUAL INVESTMENT</label>
-          <input type="number" />
+          <label> ANNUAL INVESTMENT</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              handleChangeValue("annualInvestment", event.target.value)
+            }
+          />
         </div>
       </div>
       <div className="input-group">
         <div>
-          <label htmlFor=""> EXPECTED RETURN</label>
-          <input type="number" />
+          <label> EXPECTED RETURN</label>
+          <input
+            type="number"
+            required
+            value={userInput.expecterReturn}
+            onChange={(event) =>
+              handleChangeValue("expecterReturn", event.target.value)
+            }
+          />
         </div>
         <div>
-          <label htmlFor=""> DURATION</label>
-          <input type="number" />
+          <label> DURATION</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) =>
+              handleChangeValue("duration", event.target.value)
+            }
+          />
         </div>
       </div>
     </div>
