69ce94c02e716f1a1219ee56a308419d3320d48b
Pacholekk <patryk.pacholski2001@gmail.com>
2025-02-12 11:28:31 +0100

    Add Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div id="header">
+      <img src="/investment-calculator-logo.png" alt="Grenn bag with money" />
+      <h1>Investment calculator</h1>
+    </div>
+  );
+}
